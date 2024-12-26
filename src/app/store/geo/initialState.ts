cd334f9b1f0567c53728e42932143d7c76444b2e
Alexander <al.tulin@gmail.com>
2024-12-26 12:31:11 +0800

    edit

--- src/app/store/geo/initialState.ts
+++ src/app/store/geo/initialState.ts
@@ -8,65 +8,7 @@ export interface IGeoItem {
 }
 
 export const initialState: { geoArr: IGeoItem[] | []; center: number[] } = {
-  geoArr: [
-    {
-      address: "Москва, ул. Ленина, 1",
-      latitude: 55.753215,
-      longitude: 37.61797,
-      label: "Москва метка",
-      description: "Москва ljv",
-      id: 1,
-    },
-    {
-      address: "Москва, ул. Ленина, 1",
-      latitude: 55.753215,
-      longitude: 37.61797,
-      label: "Москва метка",
-      description: "Москва ljv",
-      id: 2,
-    },
-    {
-      address: "Москва, ул. Ленина, 1",
-      latitude: 55.753215,
-      longitude: 37.61797,
-      label: "Москва метка",
-      description: "Москва ljv",
-      id: 3,
-    },
-    {
-      address: "Москва, ул. Ленина, 1",
-      latitude: 55.753215,
-      longitude: 37.61797,
-      label: "Москва метка",
-      description: "Москва ljv",
-      id: 4,
-    },
-    {
-      address: "Москва, ул. Ленина, 1",
-      latitude: 55.753215,
-      longitude: 37.61797,
-      label:
-        "Москва метка Москва метка Москва метка Москва метка Москва метка Москва метка Москва метка",
-      description: "Москва ljv",
-      id: 5,
-    },
-    {
-      address: "Москва, ул. Ленина, 1",
-      latitude: 55.753215,
-      longitude: 37.61797,
-      label: "Москва метка",
-      description: "Москва ljv",
-      id: 6,
-    },
-    {
-      address: "Москва, ул. Ленина, 1",
-      latitude: 55.753215,
-      longitude: 37.61797,
-      label: "Москва метка",
-      description: "Москва ljv",
-      id: 7,
-    },
-  ],
+  geoArr: [],
 
   center: [55.75, 37.57],
 };
